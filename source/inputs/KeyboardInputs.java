a6e62805352ae169ee268888cba39f9dcab9a2c8
Robin <dummymax2024@web.de>
2026-06-11 11:44:11 +0200

    KeyboardInputs.java, MouseInputs.java und GamePanel.java aktualisiert

---
+++ source/inputs/KeyboardInputs.java
@@ -0,0 +1,34 @@
+package source.inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import source.main.GamePanel;
+
+public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
--- source/main/GamePanel.java
+++ source/main/GamePanel.java
@@ -1,17 +1,24 @@
 package source.main;
 
 import java.awt.Dimension;
-
 import javax.swing.JPanel;
+import source.inputs.KeyboardInputs;
+import source.inputs.MouseInputs;
 
 public class GamePanel extends JPanel{ // Zeichenfläche
     
+	private MouseInputs mouseInputs;
     private Game game;
 
 	public GamePanel(Game game) {
+		mouseInputs = new MouseInputs(this);
 		this.game = game;
 		
 		setPanelSize();
+
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
 	}
 
     private void setPanelSize() {
