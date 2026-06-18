9b6a55e1a2240681f9ec987724ddb9462046ba0e
NicNac1000 <mail@supernick.de>
2026-06-18 12:26:50 +0200

    sprite sheet

--- source/utils/LoadSave.java
+++ source/utils/LoadSave.java
@@ -12,7 +12,7 @@ import source.main.Game;
 public class LoadSave {
 
 	public static final String PLAYER_ATLAS = "pig_player.png";
-	public static final String LEVEL_ATLAS = "boden.png";
+	public static final String LEVEL_ATLAS = "sprite_sheet.png";
 	public static final String LEVEL_ONE_DATA = "Level.png";
 	public static final String MENU_BUTTONS = "button_atlas.png";
 	public static final String MENU_BACKGROUND = "menu_background.png";
